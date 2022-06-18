df3f869eafe2dc1e63d5d452a33e561961813af1
YeonJae <duswo9595@naver.com>
2022-06-18 14:24:04 +0900

    useRouter적용

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
+  console.log(router);
   return (
     <nav>
       <Link href="/">
-        <a>Home</a>
+        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
       </Link>
 
       <Link href="/about">
-        <a>About</a>
+        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+          About
+        </a>
       </Link>
     </nav>
   );
